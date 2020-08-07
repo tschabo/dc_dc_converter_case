c340f7efaf97ba1cbf29eb5d45c3cbbe0c835c10
Sebastian Schmitt <mulenker@gmail.com>
2020-08-07 15:48:26 +0200

    bolts and notch

--- base.js
+++ base.js
@@ -1,3 +1,7 @@
+
+let wall = 2;
+let spaceBetweenFloorAndPlatine = 8;
+
 let platineX = 61;
 let platineY = 34;
 let platineZ = 15.5
@@ -9,27 +13,37 @@ let holeR = 1.5;
 
 let outerX = platineX + 10;
 let outerY = platineY + 10;
-let outerZ = platineZ + 2.5;
+let outerZ = platineZ + spaceBetweenFloorAndPlatine + wall;
 
-let wall = 2;
-let spaceBetweenFloorAndPlatine = 4;
 
-let cornerCubeX = (outerX - holeCenterX - holeR) / 2 + 2 * holeR + 2;
-let cornerCubeY = (outerY - holeCenterY - holeR) / 2 + 2 * holeR + 2;
+let cornerCubeX = (outerX - holeCenterX - holeR) / 2 + 2 * holeR ;
+let cornerCubeY = (outerY - holeCenterY - holeR) / 2 + 2 * holeR ;
 let cornerCubeZ = (wall + spaceBetweenFloorAndPlatine);
 
+function notch(){
+    let notchX = wall;
+    let notchY = 14; // for cable
+    let notchZ = outerZ - (wall + spaceBetweenFloorAndPlatine - 5 /*for cable*/);
+
+    return translate(
+        [0,(outerY-notchY)/2,(wall + spaceBetweenFloorAndPlatine - 5 /*for cable*/)],
+        cube({size:[notchX,notchY,notchZ]})
+    );
+}
+
 function base() {
-    return difference(
+    let b = difference(
         cube({ size: [outerX, outerY, outerZ] }),
         translate(
             [wall, wall, wall],
             cube({ size: [outerX - wall * 2, outerY - wall * 2, outerZ - wall] })
         )
     );
+    return difference(b,notch());
 }
 
 function cornerCubes() {
-    let cc = cube({ size: [cornerCubeX, cornerCubeY, cornerCubeZ] });//= cube({ size: [cornerCubeX, cornerCubeY, cornerCubeZ] });
+    let cc = cube({ size: [cornerCubeX, cornerCubeY, cornerCubeZ] });
     for(let x = 0; x < 2; x++){
         for(let y = 0; y < 2; y++){
             if(x == 0 && y == 0)
@@ -40,10 +54,29 @@ function cornerCubes() {
     return cc;
 }
 
+function bolts(){
+    let bolt = cylinder({r: holeR - 0.1, h: cornerCubeZ + 2, center: [true,true,false]}) ;
+    let all = bolt;
+    for(let x = 0; x < 2; x++){
+        for(let y = 0; y < 2; y++){
+            if(x == 0 && y == 0)
+                continue;
+            all = union(all, translate([x*holeCenterX, y*holeCenterY,0], bolt));
+        }
+    }
+    return translate([(outerX-holeCenterX)/2, (outerY-holeCenterY)/2,0],all);
+}
+
+
+
 function withCornercubes(){
     return union(base(), cornerCubes());
 }
 
+function withCornercubesAndBolts(){
+    return union(withCornercubes(), bolts());
+}
+
 function main() {
-    return withCornercubes()
+    return withCornercubesAndBolts()
 }
